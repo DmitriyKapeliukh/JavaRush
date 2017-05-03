a715cd7756fedf93fc22c83038904e31c000c6a2
Dmytro Kapeliukh <Dmytro_Kapeliukh@epam.com>
2017-05-03 09:27:15 +0300

    dad

---
+++ 2.JavaCore/src/com/javarush/task/task12/task1208/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task12.task1208;
+
+/* 
+Свобода печати
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    static void print(int a) {
+
+    }
+
+    static void print(String b) {
+
+    }
+
+    static void print(char c) {
+
+    }
+
+    static void print(int a, String b) {
+
+    }
+
+    static void print(String b, int a){
+
+    }
+}
