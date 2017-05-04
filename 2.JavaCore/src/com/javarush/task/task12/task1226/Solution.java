782a3683191fcca534da72843b411707d614f10c
Dmytro_Kapeliukh <welcome.t.t100@gmail.com>
2017-05-04 19:07:35 +0300

    ssf

--- 2.JavaCore/src/com/javarush/task/task12/task1226/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task12/task1226/Solution.java
@@ -5,16 +5,44 @@ package com.javarush.task.task12.task1226;
 */
 
 public class Solution {
-
-    public class Cat {
+    public interface Fly{
+        void fly();
     }
 
-    public class Dog {
+    public interface Climb{
+        void climb();
+    }
+
+    public interface Run{
+        void run();
+    }
+
+    public class Cat implements Climb, Run{
+        public void climb(){
+
+        }
+
+        public void run(){
+
+        }
+    }
+
+    public class Dog implements Run{
+        public void run(){
+
+        }
     }
 
     public class Tiger extends Cat {
     }
 
-    public class Duck {
+    public class Duck implements Fly, Run{
+        public void fly(){
+
+        }
+
+        public void run(){
+
+        }
     }
 }
